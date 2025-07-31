818fa238559c2b3f177e829c9838437c145b4ea2
Devmria <devmria@gmail.com>
2025-07-31 16:13:19 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# InTransition Task #4
+## Developer: devmria@gmail.com (Kuznetsova Maria)
+
+Fullstack app with:
+
+- React + Vite + Tailwind (client)
+- Express + Prisma + PostgreSQL (server)
---
+++ server/prisma/migrations/20250731115850_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "public"."UserStatus" AS ENUM ('ACTIVE', 'BLOCKED');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "status" "public"."UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "lastLoginAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
